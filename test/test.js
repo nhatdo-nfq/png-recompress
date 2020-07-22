5e841e36f855f23d87c603e53516f6cfb68bffc3
Nhat Do <ddnhat@gmail.com>
2020-07-22 11:13:00 +0700

    update

--- test/test.js
+++ test/test.js
@@ -1,13 +1,13 @@
-const path = require('path')
-const {pngRecompress} = require('..')
+const path = require('path');
+const {pngRecompress} = require('..');
 const compareSize = require('compare-size');
 const test = require('ava');
 
 test('minify a JPG', async t => {
-  const src = path.resolve(__dirname, './test.png');
-  const dest = path.resolve(__dirname, './final.png');
-  await pngRecompress(40, 80, src, dest)
-  const result = await compareSize(src, dest);
+	const src = path.resolve(__dirname, './test.png');
+	const dest = path.resolve(__dirname, './final.png');
+	await pngRecompress(40, 80, src, dest);
+	const result = await compareSize(src, dest);
 
-  t.true(result[dest] < result[src]);
+	t.true(result[dest] < result[src]);
 });
