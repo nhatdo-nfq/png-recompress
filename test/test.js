a9181ee1f3faccf52cc0cc6b3631337ed4d29ffe
Nhat Do <ddnhat@gmail.com>
2020-07-22 11:27:25 +0700

    update

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ const {pngRecompress} = require('..');
 const compareSize = require('compare-size');
 const test = require('ava');
 
-test('minify a JPG', async t => {
+test('minify a PNG', async t => {
 	const src = path.resolve(__dirname, './test.png');
 	const dest = path.resolve(__dirname, './final.png');
 	await pngRecompress(40, 80, src, dest);
